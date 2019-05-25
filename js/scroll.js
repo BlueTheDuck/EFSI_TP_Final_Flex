fef09e437fbb29a6e75bad626e1d526268f49f38
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-05-25 18:18:32 -0300

    Finished smooth scrolling and fixed character classes

---
+++ js/scroll.js
@@ -0,0 +1,39 @@
+let scroller;
+let distances = {
+    "home": window.innerHeight
+}
+
+window.onload = () => {
+    scroller = document.querySelector("#pages-wrapper");
+
+    window.location.hash = "";
+}
+
+
+function smoothScroll(to) {
+    let height = 0;
+    switch (to) {
+        case "home":
+            height = 0;
+            break;
+        case "trailer":
+            height = 1;
+            break;
+        case "cast":
+            height = 2;
+            break;
+        case "galeria":
+            height = 3;
+            break;
+        case "reviews":
+            height = 4;
+            break;
+        default:
+            console.error(`Unknown ${to}`);
+            return;
+    }
+    scroller.scroll({
+        top: height * window.innerHeight,
+        behavior: "smooth"
+    })
+}
