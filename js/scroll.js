447f038e6567a8faaf7c0e2c868a8ed8f9f7997d
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-05-29 19:20:33 -0300

    Fixed and finished

--- js/scroll.js
+++ js/scroll.js
@@ -1,43 +1,17 @@
 let scroller;
-let distances = {
-    "home": window.innerHeight
-}
-
 window.addEventListener("DOMContentLoaded",() => {
     scroller = document.querySelector("#pages-wrapper");
-
-    window.location.hash = "";
 })
 
 
 function smoothScroll(to) {
-    let height = 0;
-    switch (to) {
-        case "home":
-            height = 0;
-            break;
-        case "trailer":
-            height = 1;
-            break;
-        case "cast&crew":
-            height = 2;
-            break;
-        case "gallery":
-            height = 3;
-            break;
-        case "reviews":
-            height = 4;
-            break;
-        default:
-            console.error(`Unknown ${to}`);
-            return;
-    }
+    let height = document.querySelector(`#${to}`).offsetTop;
     if(scroller==undefined) {
         console.error("Element to scroll is undefined. Please, wait until the pages finishes loading");
         return;
     }
     scroller.scroll({
-        top: height * window.innerHeight,
+        top: height,
         behavior: "smooth"
     })
 }
